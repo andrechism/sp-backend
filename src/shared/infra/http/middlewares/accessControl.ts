06ad2970f84f29a4e8cd7ef751c4d3cc3e416978
andrechism <andrearagaocontato@gmail.com>
2023-03-21 00:17:03 -0300

    fix: verify if the origin header exists before using it

--- src/shared/infra/http/middlewares/accessControl.ts
+++ src/shared/infra/http/middlewares/accessControl.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 
 const accessControl = (request: Request, response: Response, next: NextFunction): void => {
-    response.setHeader('Access-Control-Allow-Origin', request.headers.origin)
+    if (request.headers.origin) {
+        response.setHeader('Access-Control-Allow-Origin', request.headers.origin)
+    }
 
     next()
 }
